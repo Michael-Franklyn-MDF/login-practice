c01a030b6fd1ba951831c7146ba0fdd772cbd523
Michael-Franklyn-MDF <michaeldavidfranklyn@gmail.com>
2026-01-01 17:01:54 +0300

    initial commit

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // config.php - Database configuration
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root'); // Change to your MySQL username
-define('DB_PASS', ''); // Change to your MySQL password
+define('DB_PASS', '');
 define('DB_NAME', 'user_auth_system');
 
 // Create connection
