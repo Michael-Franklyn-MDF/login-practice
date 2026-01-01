d61be5cf3c9ee755d99ed2c0fe176643203828f4
Michael-Franklyn-MDF <michaeldavidfranklyn@gmail.com>
2026-01-01 18:44:36 +0300

    chore: Update DB_PORT from 3306 to 8889.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 // config.php - Alternative for MAMP
 define('DB_HOST', '127.0.0.1');
-define('DB_PORT', 3306);
+define('DB_PORT', 8889);
 define('DB_USER', 'root');
 define('DB_PASS', 'root');
 define('DB_NAME', 'user_auth_system');
